c7a01f6f356b3e4e29053451f4619c4268d9d17e
Daniel Calaver <daniel.calaver@gmail.com>
2024-05-25 00:45:08 +0300

    add documentation v1

---
+++ doc/template_header.h
@@ -0,0 +1,42 @@
+/*
+ * template_header.h
+ *  Description: short description of header file
+ *
+ *  Created on: December 08, 2017
+ *  Author: author name
+ */
+
+#ifndef template_header_H_
+#define template_header_H_
+
+/*#################################*/
+/*       Include-Files             */
+/*#################################*/
+
+/*#################################*/
+/*       Global defines            */
+/*#################################*/
+
+/*#################################*/
+/*      Global data types          */
+/*#################################*/
+
+/*#################################*/
+/*       Global ROM data           */
+/*#################################*/
+
+/*#################################*/
+/*       Global RAM data           */
+/*#################################*/
+
+/*#################################*/
+/*   Global function declaration   */
+/*#################################*/
+/* Function name:
+   Description:
+   Function parameters:
+       param_name - description
+*/
+
+
+#endif /* template_header_H_ */
